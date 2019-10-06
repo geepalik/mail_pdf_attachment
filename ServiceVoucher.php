66f2b590f49f8922543e32476183c79f88061093
gilp <gil.palikaras@gmail.com>
2019-10-06 20:34:40 +0300

    service voucher position mapping of fields for pdf

--- ServiceVoucher.php
+++ ServiceVoucher.php
@@ -7,7 +7,20 @@
  */
 class ServiceVoucher extends PdfWrite
 {
-	protected $mapData = array();
+	protected $mapData = array(
+		'full_name' => array("170","44"),
+		'number_of_reservations' => array("25","44"),
+		'service_start_date' => array("110","44"),
+		'service_return_date' => array("60","44"),
+		'driver_name_pickup_1' => array("151","55"),
+		'pickup_location_1' => array("96","55"),
+		'dropoff_location_1' => array("47","55"),
+		'vehicle_type' => array("13","55"),
+		'driver_name_pickup_2' => array("151","66"),
+		'pickup_location_2' => array("96","66"),
+		'dropoff_location_2' => array("47","66"),
+		'price' => array("20","66"),
+	);
 
 	public function __construct( array $data, $export )
 	{
@@ -20,10 +33,31 @@ class ServiceVoucher extends PdfWrite
 	}
 
 	/**
-	 * @return mixed
+	 * @return bool|mixed
+	 * @throws Exception
 	 */
 	public function writeDataToPdf()
 	{
-		// TODO: Implement writeDataToPdf() method.
+		try{
+			$this->writeData($this->mapDataLocation('full_name'),$this->data['client']['full_name']);
+			$this->writeData($this->mapDataLocation('number_of_reservations'),$this->data['bookingData']['number_of_reservations']);
+			$this->writeData($this->mapDataLocation('service_start_date'),$this->data['bookingData']['service_start_date']);
+			$this->writeData($this->mapDataLocation('service_return_date'),$this->data['bookingData']['service_return_date']);
+			$this->writeData($this->mapDataLocation('driver_name_pickup_1'),$this->data['bookingData']['driver_name_pickup_1']);
+			$this->writeData($this->mapDataLocation('pickup_location_1'),$this->data['bookingData']['pickup_location_1']);
+			$this->writeData($this->mapDataLocation('dropoff_location_1'),$this->data['bookingData']['dropoff_location_1']);
+			$this->writeData($this->mapDataLocation('vehicle_type'),$this->data['bookingData']['vehicle_type']);
+			$this->writeData($this->mapDataLocation('driver_name_pickup_2'),$this->data['bookingData']['driver_name_pickup_2']);
+			$this->writeData($this->mapDataLocation('pickup_location_2'),$this->data['bookingData']['pickup_location_2']);
+			$this->writeData($this->mapDataLocation('dropoff_location_2'),$this->data['bookingData']['dropoff_location_2']);
+			$this->writeData($this->mapDataLocation('price'),$this->data['bookingData']['price']);
+
+			$this->pdfClass->Output($this->exportFileName, "F");
+
+			return $this->checkIfFileExists();
+
+		}catch (Exception $exception){
+			throw new Exception($exception->getMessage());
+		}
 	}
 }
