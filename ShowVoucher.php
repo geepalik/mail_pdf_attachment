33838aa54bb56a9c72aba4168b8676f6b2bf2b2a
gilp <gil.palikaras@gmail.com>
2019-10-06 20:57:44 +0300

    remove unnecessary variables read vars from new format of incoming data

--- ShowVoucher.php
+++ ShowVoucher.php
@@ -49,25 +49,15 @@ class ShowVoucher extends PdfWrite
 	public function writeDataToPdf()
 	{
 		try{
-			$fullNameCoords = $this->mapDataLocation('full_name');
-			$reservesCoords = $this->mapDataLocation('number_of_reservations');
-			$tableCoords = $this->mapDataLocation('table');
-			$dateCoords = $this->mapDataLocation('date_of_reservation');
-			$clubNameCoords = $this->mapDataLocation('club_name');
-			$clubAddressCoords = $this->mapDataLocation('club_address');
-			$clubOpenTimeCoords = $this->mapDataLocation('open_time');
-			$clubPriceCoords = $this->mapDataLocation('price');
-			$clubSingersCoords = $this->mapDataLocation('singers');
-
-			$this->writeData($fullNameCoords,$this->data['client']['full_name']);
-			$this->writeData($reservesCoords,$this->data['client']['number_of_reservations']);
-			$this->writeData($tableCoords,$this->data['client']['table']);
-			$this->writeData($dateCoords,$this->data['client']['date_of_reservation']);
-			$this->writeData($clubNameCoords,$this->data['club']['club_name']);
-			$this->writeData($clubAddressCoords,$this->data['club']['club_address']);
-			$this->writeData($clubOpenTimeCoords,$this->data['club']['open_time']);
-			$this->writeData($clubPriceCoords,$this->data['club']['price']);
-			$this->writeData($clubSingersCoords,$this->data['club']['singers']);
+			$this->writeData($this->mapDataLocation('full_name'),$this->data['client']['full_name']);
+			$this->writeData($this->mapDataLocation('number_of_reservations'),$this->data['bookingData']['number_of_reservations']);
+			$this->writeData($this->mapDataLocation('table'),$this->data['bookingData']['table']);
+			$this->writeData($this->mapDataLocation('date_of_reservation'),$this->data['bookingData']['date_of_reservation']);
+			$this->writeData($this->mapDataLocation('club_name'),$this->data['bookingData']['club_name']);
+			$this->writeData($this->mapDataLocation('club_address'),$this->data['bookingData']['club_address']);
+			$this->writeData($this->mapDataLocation('open_time'),$this->data['bookingData']['open_time']);
+			$this->writeData($this->mapDataLocation('price'),$this->data['bookingData']['price']);
+			$this->writeData($this->mapDataLocation('singers'),$this->data['bookingData']['singers']);
 
 			$this->pdfClass->Output($this->exportFileName, "F");
 
